c8d2aebb9d704f4aaae51454e5ad6cdb578ce901
CLLCode Github <49545285+cleatherbury@users.noreply.github.com>
2023-07-28 15:36:52 -0500

    push728

--- from_log_create_dictionary_and_csv.py
+++ from_log_create_dictionary_and_csv.py
@@ -5,12 +5,14 @@ import sys
 import csv
 import os
 import operator
-
+#Initialize dictionaries
 error_messages = {}
 per_user = {}
+#Defines sys.argv[] for the logfile
 log_file = sys.argv[1]
 
 def create_dictionary(log_file):
+  '''This code creates a dictionary from a logfile'''
   with open(log_file, mode='r',encoding='UTF-8') as file:
     lines = file.readlines()
     for line in lines:
@@ -34,19 +36,22 @@ def create_dictionary(log_file):
   return error_messages, per_user
 
 def create_csv_files():
+  '''This code defines dictionaries, sorts the them, and creates 2 log_report csv files'''
   dictionary = create_dictionary(log_file)
   error_dictionary = dictionary[0]
   user_dictionary = dictionary[1]
   error_dictionary = sorted(error_dictionary.items(), key = operator.itemgetter(1), reverse = True)
   user_dictionary = sorted(user_dictionary.items())
-
+  print(dictionary)
+  print(error_dictionary, user_dictionary)
+  #openning error_message file to write message data
   with open("error_message.csv", 'w') as file:
     file.write("Error,Count\n")
     for error in error_dictionary:
       a,b = error
       file.write(str(a)+','+str(b)+'\n')
   file.close()
-
+  #openning error_message file to write statistics data
   with open("user_statistics.csv", 'w') as file:
     file.write("Username,INFO,ERROR\n")
     for stats in user_dictionary:
@@ -55,4 +60,6 @@ def create_csv_files():
   file.close()
 
 if __name__ == '__main__':
+  '''Here we call the main function to create csv files and define the log_file.'''
+  log_file = '/Users/chris/Documents/GitHub/from_log_to_dictionary_and_csv/syslog.log'
   create_csv_files()
